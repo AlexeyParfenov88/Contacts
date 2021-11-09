56bfe8e101782fb37f8ef9959ffb6b04c6235608
Alexey Parfenov <aiparfenov88@gmail.com>
2021-11-10 01:41:21 +0300

    Minor fixes

--- Contacts/ContactsData.swift
+++ Contacts/ContactsData.swift
@@ -7,43 +7,29 @@
 
 import Foundation
 
-struct PersonsData {
+class DataManager{
     
-    static let sharedInstance = PersonsData()
+    static let contacsData = DataManager()
     
     var persons: [Person]
     
     private var firstNames = [
-        "Barry",
-        "Bruce",
-        "Arthur",
-        "Hal",
-        "Diana"
+        "Barry", "Bruce", "Arthur", "Hal", "Diana"
     ]
     
     private var surNames = [
-        "Allen",
-        "Wayne",
-        "Curry",
-        "Jordan",
-        "Prince"    ]
+        "Allen", "Wayne", "Curry", "Jordan", "Prince"
+    ]
     
     private var emails = [
-        "Flash@dc.com",
-        "Batman@dc.com",
-        "Aquman@dc.com",
-        "Greenlantern@dc.com",
-        "Wonderwoman@dc.com"
+        "Flash@dc.com", "Batman@dc.com", "Aquman@dc.com",
+        "Greenlantern@dc.com", "Wonderwoman@dc.com"
     ]
     
     private var phoneNumbers = [
-        "89991234567",
-        "89037654321",
-        "89161112233",
-        "89097776655",
-        "89773335577"
+        "89991234567", "89037654321", "89161112233", "89097776655", "89773335577"
     ]
-
+    
     init() {
         persons = [Person]()
         
@@ -53,9 +39,11 @@ struct PersonsData {
         phoneNumbers.shuffle()
         
         //changed name for firstName
-        for ((firstName, surName), (email, phoneNumber)) in zip(zip(firstNames, surNames), zip(emails, phoneNumbers)) {
+        for ((firstName, surName), (email, phoneNumber)) in
+                zip(zip(firstNames, surNames), zip(emails, phoneNumbers)) {
             
-            let person = Person(firstName: firstName, surName: surName, email: email, phoneNumber: phoneNumber)
+            let person = Person(firstName: firstName, surName: surName,
+                                email: email, phoneNumber: phoneNumber)
             
             self.persons.append(person)
         }
